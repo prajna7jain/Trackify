ec2c7403b2182f9220eaf293d7a3af9e2fc1cd47
Ayush-alt-sys <igayushacharya@gmail.com>
2025-06-07 23:11:46 +0530

    Fix some Error in code

--- expTrack.js
+++ expTrack.js
@@ -1,23 +1,24 @@
-const user=localStorage.getItem("User-info");
+const user = localStorage.getItem("User-info");
 let useData = JSON.parse(user);
 console.log(useData.name);
 console.log(useData.balance);
 
-document.getElementById('user-name').innerText = useData.name;
-document.getElementById('user-bal').innerText = useData.balance;
+document.getElementById("user-name").innerText = useData.name;
+document.getElementById("user-bal").innerText = useData.balance;
 
+const editBtn = document.getElementById("edit-btn");
+const editSection = document.getElementById("edit-section");
+const addBalBtn = document.getElementById("addbal-btn");
+const editBalBtn = document.getElementById("editbal-btn");
 
-const editBtn= document.getElementById('edit-btn');
-const editSection = document.getElementById('edit-section');
-const addBalBtn = document.getElementById('addbal-btn');
-const editBalBtn = document.getElementById('editbal-btn');
+const inputFields = document.getElementById("input-fields-section");
 
-
- editBtn.addEventListener('click', () => {
-    console.log("clicked");
-            addBalBtn.classList.add('hidden-section');
-            editBalBtn.classList.add('hidden-section');
-
-            
-        });
-
+editBtn.addEventListener("click", () => {
+  console.log("clicked");
+//   Remove Madu edanna 
+  addBalBtn.classList.add("hidden-section");
+  editBalBtn.classList.add("hidden-section");
+  
+//   E line aa add madu 
+// inputFields.classList.remove("hidden-section");
+});

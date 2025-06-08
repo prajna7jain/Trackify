b53193093a175ad3583cd1d33662a045b3cc0391
Ayush-alt-sys <igayushacharya@gmail.com>
2025-06-08 16:22:35 +0530

    Update code

--- expTrack.js
+++ expTrack.js
@@ -1,40 +1,35 @@
-const user1=localStorage.getItem("User-info1");
+const user1 = localStorage.getItem("User-info1");
 let useData1 = JSON.parse(user1);
 console.log(useData1.name);
 console.log(useData1.balance);
 
-document.getElementById('user-name').innerText = useData1.name;
-document.getElementById('user-bal').innerText = useData1.balance;
+document.getElementById("user-name").innerText = useData1.name;
+document.getElementById("user-bal").innerText = useData1.balance;
 
+const editBtn = document.getElementById("edit-btn");
+const editSection = document.getElementById("edit-section");
+const addbalBtn = document.getElementById("addbal-btn");
+const editbalBtn = document.getElementById("editbal-btn");
 
-const editBtn= document.getElementById('edit-btn');
-const editSection = document.getElementById('edit-section');
-const addbalBtn = document.getElementById('addbal-btn');
-const editbalBtn = document.getElementById('editbal-btn');
+const inputFields = document.getElementById("input-fields-section");
 
-
- editBtn.addEventListener('click', () => {
-    console.log("clicked");
-            addbalBtn.classList.add('hidden-section1');
-            editbalBtn.classList.add('hidden-section1');
- 
-        });
-
-userBalance=document.getElementById('user-bal');
-let addExpense=document.getElementById("add-exp-btn");
-addExpense.addEventListener("click",()=>{
-     console.log("clicked");
-     alert("in");
-    let expName=document.getElementById("expense-name").value;
-    let price=document.getElementById("price2").value;
-    if(expName !="" && price!=""){
-         if(price<=userBalance){
-            userBalance=userBalance-price;
-            userBalance.innerText = userBalance;
-
-         }
-    }
-
+editBtn.addEventListener("click", () => {
+  console.log("clicked");
+  inputFields.classList.remove("hidden-section1");
 });
 
-
+userBalance = document.getElementById("user-bal");
+let addExpense = document.getElementById("add-exp-btn");
+addExpense.addEventListener("click", () => {
+  console.log("clicked");
+  let expName = document.getElementById("expense-name").value;
+  let price = document.getElementById("price2").value;
+  if (expName != "" && price != "") {
+    console.log("Enter");
+    if (price <= userBalance) {
+      document.getElementById("user-bal").innerText = useData1.balance - price;
+      // userBalance=userBalance-price;
+      // userBalance.innerText = userBalance;
+    }
+  }
+});
